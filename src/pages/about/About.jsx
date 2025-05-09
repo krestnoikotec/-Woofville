803c13b536f092d0d038bab249ec6ced9bec7ce5
Maistrenko Roman <maistrenko.roman@gmail.com>
2025-05-09 16:11:29 +0300

    09.05.2025

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -18,6 +18,25 @@ const About = () => {
                 {/*<img className={styles.aboutDogImage1} src="https://media.istockphoto.com/id/1041987488/photo/cute-dog-put-his-face-on-his-knees-to-the-man-and-smiling-from-the-hands-scratching-her-ear.jpg?s=612x612&w=0&k=20&c=NKGf8nmXVdksmNS0Ay696cVPNSIfCJJ1yu_y9jFGBsM=" alt="smillig dog"/>*/}
                 {/*<img className={styles.aboutDogImage2} src="https://media.istockphoto.com/id/1155030342/uk/%D1%84%D0%BE%D1%82%D0%BE/%D1%81%D0%BE%D0%B1%D0%B0%D0%BA%D0%B0-%D0%BF%D0%BE%D0%B4%D0%BE%D1%80%D0%BE%D0%B6-%D0%BD%D0%B0-%D0%BC%D0%B0%D1%88%D0%B8%D0%BD%D1%96.jpg?s=612x612&w=0&k=20&c=YtOaHQpZelmAYt5eGFZnBzwv5qd4C9Zzhb4yy6UJgfY=" alt="dog in the car"/>*/}
             </section>
+            <section className={styles.infoSection}>
+                <h3 className={styles.infoTitle}>Discover Joy Through Dog Photos</h3>
+                <p className={styles.infoSubtitle}>Welcome to Woofville, where we celebrate the joy and warmth that dogs bring to our lives through captivating images that uplift your spirit and brighten your day.</p>
+                <div className={styles.infoStats}>
+                    <div className={styles.statItem}>
+                        <h3 className={styles.statNumber}>150+</h3>
+                        <p className={styles.statLabel}>Love Dogs</p>
+                    </div>
+                    <div className={styles.statItem}>
+                        <h3 className={styles.statNumber}>15</h3>
+                        <p className={styles.statLabel}>Join Our Community</p>
+                    </div>
+                </div>
+                <img
+                    className={styles.infoImage}
+                    src="https://images.unsplash.com/photo-1584720611279-4bafbc05600a?auto=format&fit=clip&w=1224&q=100"
+                    alt="A fluffy dog with curly fur and big eyes gazes attentively at the camera, resting on soft, neutral-toned blankets. The lighting is warm and cozy, highlighting the dog's mischievous yet endearing expression."
+                />
+            </section>
         </div>
     );
 };
